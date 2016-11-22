5c2eef5ed74761f32013ad35ab7d2c5b127f20ae
ogiwara <yushiogiwara@gmail.com>
2016-11-22 22:52:02 +0900

    MiNETPlugin

---
+++ MiNETplugins/TestPlugin/TestPlugin/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MiNET;
+using MiNET.Plugins;
+using MiNET.Plugins.Attributes;
+using log4net;
+
+namespace TestPlugin
+{
+    [Plugin(Author = "ogiwara",Description = "This is my first MiNET plugin",PluginName = "Test",PluginVersion = "1.0")]//引数…
+    public class Class1 : Plugin
+    {
+
+        protected override void OnEnable()
+        {
+            Console.WriteLine("Plugin loaded.");
+
+            var server = Context.Server;
+
+            server.PlayerFactory.PlayerCreated += (sender,args) =>
+            {
+                Player player = args.Player;
+                player.PlayerJoin += (o, eventArgs) => eventArgs.Player.Level.BroadcastMessage($"{eventArgs.Player.Username}" + "が参加しました");
+            };  
+        }
+    }
+}
---
+++ MiNETplugins/TestPlugin/TestPlugin/Class2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MiNET.Plugins;
+using MiNET.Plugins.Attributes;
+using MiNET;
+using System.Threading.Tasks;
+
+namespace TestPlugin
+{
+    class Class2//コマンド
+    {
+        [Command(Name = "test1",Description = "testコマンド1")]
+        public void aha(Player source)
+        {
+            source.SendMessage("test1コマンドが実行");
+        }
+    }
+}
