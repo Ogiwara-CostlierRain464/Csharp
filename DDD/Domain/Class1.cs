faf111d46346208dcf8199a46e0e1f50ea64579e
ogiwara <ogiwara.dev@gmail.com>
2018-04-08 10:57:42 +0900

    .NET Coreむけに変更した

--- DDD/Domain/Class1.cs
+++ DDD/Domain/Class1.cs
@@ -1,69 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Diagnostics;
-using System.Collections;
 using System.Reflection;
+using System.Linq;
 
-namespace DDD　{
-    class Program {
-
-        static void Main(string[] args){
-
-        }
-    }
-
-    public class Customer {
-
+namespace Domain
+{
+    public class Customer{
         private int _customerNumber = 0;
-        public int CustomerNumber {
+        public int CustomerNumber
+        {
             get { return _customerNumber; }
         }
 
-        public override bool Equals(object obj) {
-            if (!(obj is Customer)) {
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Customer))
+            {
                 return false;
             }
             return (obj as Customer).CustomerNumber == _customerNumber;
         }
     }
 
-    public class Order {
+    public class Order
+    {
 
         public readonly Customer Customer;
         private DateTime _orderDate;
         private int _orderNumber;
 
-        public Order(Customer customer) {
+        public Order(Customer customer)
+        {
             Customer = customer;
             _orderDate = DateTime.Now;
             _orderNumber = 0;
         }
 
-        public DateTime OrderDate {
+        public DateTime OrderDate
+        {
             get { return _orderDate; }
         }
 
-        public int OrderNumber {
+        public int OrderNumber
+        {
             get { return _orderNumber; }
         }
 
-        public int TotalAmount {
+        public int TotalAmount
+        {
             get { return 0; }
         }
 
-        public void AddOrderLine(OrderLine ol) {
+        public void AddOrderLine(OrderLine ol)
+        {
             throw new NotImplementedException();
         }
     }
 
-    public class OrderRepositoryFake: IOrderRepository {
+
+    public class OrderRepositoryFake : IOrderRepository
+    {
 
         private List<Order> _theOrders = new List<Order>();
 
-        public void AddOrder(Order order) {
+        public void AddOrder(Order order)
+        {
             int theNumberOfOrdersBefore = _theOrders.Count;
 
             _theOrders.Add(order);
@@ -71,35 +72,41 @@ namespace DDD　{
             //MyTrace.Assert(theNumberOfOrdersBefore == _theOrders.Count);
         }
 
-        public Order GetOrder(int orderNumber) {
-            return _theOrders.FirstOrDefault(e => 
-                e.OrderNumber == orderNumber
-            );
-            
+        public Order GetOrder(int orderNumber)
+        {
+            return _theOrders.FirstOrDefault(e => e.OrderNumber == orderNumber);
+
         }
 
-        public List<Order> GetOrders(Customer c) {
-            return _theOrders.Where(e =>  e.Customer.Equals(c)).ToList();
+        public List<Order> GetOrders(Customer c)
+        {
+            return _theOrders.Where(e => e.Customer.Equals(c)).ToList();
         }
     }
 
-    public interface IOrderRepository {
+    public interface IOrderRepository
+    {
         void AddOrder(Order order);
         Order GetOrder(int orderNumber);
         List<Order> GetOrders(Customer c);
     }
 
 
-    public class MyTrace {
-        public static void Assert(bool assert) {
-            if (assert == false) {
+    public class MyTrace
+    {
+        public static void Assert(bool assert)
+        {
+            if (assert == false)
+            {
                 throw new Exception("Assertation Failed");
             }
         }
     }
 
-    public class RepositoryHelper {
-        public static void SetFieldWhenReconstitutingFromPersistence(object instance, string fieldName, object newValue) {
+    public class RepositoryHelper
+    {
+        public static void SetFieldWhenReconstitutingFromPersistence(object instance, string fieldName, object newValue)
+        {
             Type t = instance.GetType();
             FieldInfo f = t.GetField(fieldName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
 
@@ -107,44 +114,50 @@ namespace DDD　{
         }
     }
 
-    //自分で書いた
-    public class Product {
+    public class Product
+    {
         private string _name;
         private double _price;
 
-        public Product(string name, double price) {
+        public Product(string name, double price)
+        {
             _name = name;
             _price = price;
         }
 
-        public double UnitPrice {
+        public double UnitPrice
+        {
             get { return _price; }
         }
 
-        public string Description {
+        public string Description
+        {
             get { return _name; }
         }
     }
 
-    //自動生成
-    public class OrderLine {
+    public class OrderLine
+    {
         public double Price = 0;
         public int NumberOfUnits;
-    
+
         private Product _product;
 
-        public OrderLine(Product product) {
+        public OrderLine(Product product)
+        {
             _product = product;
             Price = product.UnitPrice;
         }
 
-        
 
-        public Product Product {
+
+        public Product Product
+        {
             get { return _product; }
         }
 
-        public double TotalAmout {
+        public double TotalAmout
+        {
             get { return Price * NumberOfUnits; }
         }
     }
--- DDD/UnitTest1/UnitTest1.cs
+++ DDD/UnitTest1/UnitTest1.cs
@@ -1,91 +1,102 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using DDD;
+using System;
+using Xunit;
+using Domain;
 
-namespace UnitTestProject1 {
-    [TestClass]
-    public class UnitTest1 {
+namespace UnitTest1
+{
+    public class UnitTest1
+    {
 
         private IOrderRepository _repository;
 
-        [TestInitialize]
-        public void SetUp() {
+
+        public UnitTest1()
+        {
             _repository = new OrderRepositoryFake();
         }
 
-        [TestMethod]
-        public void OrderHasCustomer() {
+        [Fact]
+        public void OrderHasCustomer()
+        {
             Order o = new Order(new Customer());
 
-            Assert.IsNotNull(o.Customer);
+            Assert.NotNull(o.Customer);
         }
 
-        [TestMethod]
-        public void OrderDateIsCurrentAfterCreation() {
+        [Fact]
+        public void OrderDateIsCurrentAfterCreation()
+        {
             var theTimeBefore = DateTime.Now.AddMilliseconds(-1);
             Order o = new Order(new Customer());
 
-            Assert.IsTrue(o.OrderDate > theTimeBefore);
-            Assert.IsTrue(o.OrderDate < DateTime.Now.AddMilliseconds(1));
+            Assert.True(o.OrderDate > theTimeBefore);
+            Assert.True(o.OrderDate < DateTime.Now.AddMilliseconds(1));
         }
 
-        [TestMethod]
-        public void OrderNumberIsZeroAfterCreation() {
+        [Fact]
+        public void OrderNumberIsZeroAfterCreation()
+        {
             Order o = new Order(new Customer());
 
-            Assert.AreEqual(0, o.OrderNumber);
+            Assert.Equal(0, o.OrderNumber);
         }
 
 
         //[Ignore]
-        [TestMethod]
-        public void OrderNumberCantBeZeroAfterReconstitution() {
+        [Fact]
+        public void OrderNumberCantBeZeroAfterReconstitution()
+        {
             int theOrderNumber = 42;
             _FakeAnOrder(theOrderNumber, new Customer(), _repository);
 
             Order o = _repository.GetOrder(theOrderNumber);
 
-            Assert.AreEqual(theOrderNumber, o.OrderNumber);
+            Assert.Equal(theOrderNumber, o.OrderNumber);
         }
 
-        private void _FakeAnOrder(int orderNumber, Customer c, IOrderRepository repository) {
+        private void _FakeAnOrder(int orderNumber, Customer c, IOrderRepository repository)
+        {
             var o = new Order(c);
             RepositoryHelper.SetFieldWhenReconstitutingFromPersistence(o, "_orderNumber", orderNumber);
             repository.AddOrder(o);
         }
 
-        [TestMethod]
-        public void CanAddOrder() {
+        [Fact]
+        public void CanAddOrder()
+        {
             _repository.AddOrder(new Order(new Customer()));
         }
 
-        [TestMethod]
-        public void CanFindOrdersViaCustomer() {
+        [Fact]
+        public void CanFindOrdersViaCustomer()
+        {
             var c = _FakeACustomer(1);
             _FakeAnOrder(42, c, _repository);
             _FakeAnOrder(12, _FakeACustomer(2), _repository);
             _FakeAnOrder(3, _FakeACustomer(1), _repository);
 
-            Assert.AreEqual(2, _repository.GetOrders(c).Count);
+            Assert.Equal(2, _repository.GetOrders(c).Count);
 
         }
 
-        private Customer _FakeACustomer(int customerNumber) {
+        private Customer _FakeACustomer(int customerNumber)
+        {
             var c = new Customer();
             RepositoryHelper.SetFieldWhenReconstitutingFromPersistence(c, "_customerNumber", customerNumber);
 
             return c;
         }
 
-        [TestMethod]
-        public void EmptyOrderHasZeroForTotalAmount() {
+        [Fact]
+        public void EmptyOrderHasZeroForTotalAmount()
+        {
             var o = new Order(new Customer());
-            Assert.AreEqual(0, o.TotalAmount);
+            Assert.Equal(0, o.TotalAmount);
         }
 
-        [Ignore]
-        [TestMethod]
-        public void OrderWithLinesHasTotalAmount() {
+        [Fact(Skip = "reason")]
+        public void OrderWithLinesHasTotalAmount()
+        {
             var o = new Order(new Customer());
 
             var ol = new OrderLine(new Product("Chair", 52.00));
@@ -96,32 +107,36 @@ namespace UnitTestProject1 {
             ol2.NumberOfUnits = 3;
             o.AddOrderLine(ol2);
 
-            Assert.AreEqual(104.00 + 345.00, o.TotalAmount);
-        
+            Assert.Equal(104.00 + 345.00, o.TotalAmount);
+
         }
 
-        [TestMethod]
-        public void OrderLineGetsDefaultPrice() {
+        [Fact]
+        public void OrderLineGetsDefaultPrice()
+        {
             var p = new Product("Chair", 52.00);
             var ol = new OrderLine(p);
 
-            Assert.AreEqual(52.00, ol.Price);
+            Assert.Equal(52.00, ol.Price);
         }
 
-        [TestMethod]
-        public void OrderLineHasTotalAmount() {
+        [Fact]
+        public void OrderLineHasTotalAmount()
+        {
             var ol = new OrderLine(new Product("Chair", 52.00));
             ol.NumberOfUnits = 2;
 
-            Assert.AreEqual(104.00, ol.TotalAmout);
+            Assert.Equal(104.00, ol.TotalAmout);
         }
 
-        [TestMethod]
-        public void CanAddOrderLine() {
+        [Fact]
+        public void CanAddOrderLine()
+        {
             var o = new Order(new Customer());
-            
+
 
         }
 
     }
 }
+
